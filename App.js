17839aee6344ad7550c9d789f9a5dfa960e99e2f
Niko <t1koni02@students.oamk.fi>
2023-08-29 15:45:07 +0300

    excercise done

--- App.js
+++ App.js
@@ -1,10 +1,38 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
 export default function App() {
+  const [age, setAge] = useState('');
+  const [heartRateLimits, setHeartRateLimits] = useState('');
+
+  const calculateHeartRateLimits = (Age) => {
+    if (Age === '') {
+      setHeartRateLimits('');
+      return;
+    }
+
+    const lowerLimit = Math.round((220 - parseInt(Age)) * 0.65);
+    const upperLimit = Math.round((220 - parseInt(Age)) * 0.85);
+
+    setHeartRateLimits(`Hr limits\n${lowerLimit} - ${upperLimit}`);
+  };
+
+  const handleAgeChange = (Age) => {
+    setAge(Age);
+    calculateHeartRateLimits(Age);
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Age</Text>
+      <TextInput
+        style={styles.input}
+        onChangeText={handleAgeChange}
+        value={age}
+        keyboardType="numeric"
+      />
+      <Text style={styles.heartRateLimits}>{heartRateLimits}</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -16,5 +44,17 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    padding: 20,
+  },
+  input: {
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    marginBottom: 10,
+    paddingHorizontal: 10,
+  },
+  heartRateLimits: {
+    marginTop: 10,
+    textAlign: 'center',
   },
 });
